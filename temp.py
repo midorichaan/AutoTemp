04a22b113bc687b07334ed87d91bcdf23c77d69e
midorichan <midorichan@adm.rspnet.jp>
2021-09-02 15:36:57 +0900

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,5 +1,4 @@
 import random
-import sys
 import asyncio
 
 loop = asyncio.get_event_loop()
@@ -14,12 +13,7 @@ def _generate_temp(min:float=36.0, max:float=37.0, days:int=1):
         return [round(random.uniform(min, max), 2) for i in range(int(days))]
 
 def get_temp():
-    args = sys.argv
-    
-    if not args:
-        return _generate_temp()
-    else:
-        return _generate_temp(args[0], args[1], args[3])
+    return _generate_temp()
 
 if __name__ == "__main__":
     loop.run_until_complete(get_temp())
