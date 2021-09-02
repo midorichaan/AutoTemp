f94f6a286903d03f3fcf769c5b468ece842799bd
midorichan <midorichan@adm.rspnet.jp>
2021-09-02 15:37:37 +0900

    Update temp.py

--- temp.py
+++ temp.py
@@ -13,7 +13,7 @@ def _generate_temp(min:float=36.0, max:float=37.0, days:int=1):
         return [round(random.uniform(min, max), 2) for i in range(int(days))]
 
 async def get_temp():
-    return _generate_temp()
+    print(_generate_temp())
 
 if __name__ == "__main__":
     loop.run_until_complete(get_temp())
